09dec4d99af5005f8c1b6bbae05713b1e471a3c8
Igor <pa220282kiv@gmail.com>
2025-06-23 16:28:53 +0300

    Fix CarCard height

--- src/components/CarCard/CarCard.jsx
+++ src/components/CarCard/CarCard.jsx
@@ -17,37 +17,40 @@ const CarCard = ({
 }) => {
   return (
     <div className={css.carCard}>
-      <img className={css.imgCard} src={img} alt={description} />
-      <div className={css.textThumb}>
-        <p className={css.text}>
-          {brand}
-          <span className={css.highlight}> {model}, </span>
-          {year}
-        </p>
-        ${rentalPrice}
-      </div>
-      <ul className={clsx(css.addressThumb, css.addressMargin)}>
-        <li>
-          <p className={css.address}>{address.split(',').slice(1, 2)}</p>
-        </li>
-        <li>
-          <p className={css.address}>{address.split(',').slice(2, 3)}</p>
-        </li>
-        <li>
-          <p className={css.address}>{rentalCompany}</p>
-        </li>
-      </ul>
-
-      <ul className={clsx(css.addressThumb, css.addressButtomMargin)}>
-        <li>
-          <p className={css.address}>{type}</p>
-        </li>
-        <li>
-          <p className={clsx(css.address, css.cleanBorder)}>
-            {mileage.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')} km
+      <div>
+        <img className={css.imgCard} src={img} alt={description} />
+        <div className={css.textThumb}>
+          <p className={css.text}>
+            {brand}
+            <span className={css.highlight}> {model}, </span>
+            {year}
           </p>
-        </li>
-      </ul>
+          <p className={css.text}>${rentalPrice}</p>
+        </div>
+        <ul className={clsx(css.addressThumb, css.addressMargin)}>
+          <li>
+            <p className={css.address}>{address.split(',').slice(1, 2)}</p>
+          </li>
+          <li>
+            <p className={css.address}>{address.split(',').slice(2, 3)}</p>
+          </li>
+          <li>
+            <p className={css.address}>{rentalCompany}</p>
+          </li>
+        </ul>
+
+        <ul className={css.addressThumb}>
+          <li>
+            <p className={css.address}>{type}</p>
+          </li>
+          <li>
+            <p className={clsx(css.address, css.cleanBorder)}>
+              {mileage.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')} km
+            </p>
+          </li>
+        </ul>
+      </div>
+
       <Button width="wide" color="blue" to={'/catalog/' + id.toString()}>
         Read more
       </Button>
